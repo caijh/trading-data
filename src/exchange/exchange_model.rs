47d924f1026b93b528b6556daedef6409d521b10
johnhuitsai <caiqizhe@gmail.com>
2025-04-13 22:15:12 +0800

    feat(exchange): 为 Exchange枚举类型添加额外的方法和文档注释
    - 添加 from_str 方法，用于将字符串转换为交易所枚举类型
    - 实现 time_zone、int_code 和 stock_code_suffix 方法
    - 为枚举类型和方法添加详细的文档注释

--- src/exchange/exchange_model.rs
+++ src/exchange/exchange_model.rs
@@ -3,7 +3,6 @@ use std::error::Error;
 use std::str::FromStr;
 
 /// 股票交易所
-/// 枚举中的每个变体都包含一个String类型，用于存放交易所的名称或代码
 pub enum Exchange {
     /// 上交所
     SSE,
@@ -29,6 +28,21 @@ impl AsRef<str> for Exchange {
 impl FromStr for Exchange {
     type Err = Box<dyn Error>;
 
+    /// 将字符串转换为交易所枚举类型
+    ///
+    /// 此函数旨在解析代表不同交易所的字符串代码，并将其转换为相应的枚举值
+    /// 它首先将输入字符串转换为大写，然后与预定义的交易所代码进行匹配
+    /// 如果匹配成功，则返回对应的交易所枚举值；如果匹配失败，则返回一个错误
+    ///
+    /// # 参数
+    /// * `s`: 一个代表交易所代码的字符串切片
+    ///
+    /// # 返回值
+    /// * `Result<Self, Self::Err>`: 成功时返回对应的交易所枚举值，失败时返回一个自定义错误
+    ///
+    /// # 示例
+    /// let exchange = Exchange::from_str("SSE").unwrap(); 
+    /// assert_eq!(exchange, Exchange::SSE); 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_uppercase().as_str() {
             "SSE" => Ok(Exchange::SSE),
@@ -40,9 +54,16 @@ impl FromStr for Exchange {
     }
 }
 
+/// 交易所枚举类型实现
 impl Exchange {
+    /// 定义所有支持的交易所常量数组
     pub const VALUES: [Self; 4] = [Self::SSE, Self::SZSE, Self::HKEX, Self::NASDAQ];
 
+    /// 返回交易所对应的时间区
+    ///
+    /// # Returns
+    ///
+    /// * `Tz` - 交易所的时间区
     pub fn time_zone(&self) -> Tz {
         match self {
             Exchange::SSE => chrono_tz::Asia::Chongqing,
@@ -52,6 +73,11 @@ impl Exchange {
         }
     }
 
+    /// 返回交易所的内部代码
+    ///
+    /// # Returns
+    ///
+    /// * `usize` - 交易所的内部代码
     pub fn int_code(&self) -> usize {
         match self {
             Exchange::SSE => 10,
@@ -61,6 +87,11 @@ impl Exchange {
         }
     }
 
+    /// 返回交易所的股票代码后缀
+    ///
+    /// # Returns
+    ///
+    /// * `String` - 交易所的股票代码后缀
     pub fn stock_code_suffix(&self) -> String {
         match self {
             Exchange::SSE => ".SH".to_string(),
@@ -70,3 +101,4 @@ impl Exchange {
         }
     }
 }
+
