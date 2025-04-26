6d89963626adf6d479486d260d51f881b8f1d58a
johnhuitsai <caiqizhe@gmail.com>
2025-04-26 22:40:54 +0800

    refactor(stock): 重构获取股票日常价格逻辑
    - 优化缓存命中和未命中处理流程，提高代码可读性
    -减少嵌套的 match 表达式，使代码结构更清晰
    - 添加注释解释代码逻辑，便于维护和理解

--- src/stock/stock_svc.rs
+++ src/stock/stock_svc.rs
@@ -151,41 +151,43 @@ pub async fn get_stock_daily_price_from_cache(
     let mut con = client.get_connection()?;
     let key = "Stock:Price:K:D:".to_string() + &stock.code;
     let value = con.get::<&str, Option<String>>(&key)?;
-    match value {
-        None => {
-            let exchange = Exchange::from_str(&stock.exchange)?;
-            let date = Utc::now()
-                .with_timezone(&exchange.time_zone())
-                .format("%Y%m%d")
-                .to_string()
-                .parse::<u64>()
-                .unwrap();
-            let sync_record = sync_record_dao::get_sync_record(stock).await?;
-            let mut updated: bool = false;
-            if let Some(sync_record) = sync_record {
-                updated = sync_record.date == date && sync_record.updated;
-            }
-            let prices = if updated {
-                let prices = stock_price_dao::get_stock_prices(stock).await?;
-                let now = Utc::now().with_timezone(&exchange.time_zone());
-                let seconds = 3600 * 24 - now.num_seconds_from_midnight();
-                con.set_ex::<&str, String, String>(
-                    &key,
-                    serde_json::to_string(&prices).unwrap(),
-                    seconds as u64,
-                )?;
-                prices
-            } else {
-                Vec::new()
-            };
-            Ok(prices)
-        }
-        Some(value) => {
-            info!("Get stock daily price from cache, code = {}", stock.code);
-            let prices: Vec<stock_price_model::Model> = serde_json::from_str(&value).unwrap();
-            Ok(prices)
-        }
+
+    // 缓存命中，直接返回结果
+    if let Some(value) = value {
+        info!("Get stock daily price from cache, code = {}", stock.code);
+        let prices: Vec<stock_price_model::Model> = serde_json::from_str(&value).unwrap();
+        return Ok(prices);
     }
+
+    // 缓存未命中，执行同步逻辑
+    let exchange = Exchange::from_str(&stock.exchange)?;
+    let date = Utc::now()
+        .with_timezone(&exchange.time_zone())
+        .format("%Y%m%d")
+        .to_string()
+        .parse::<u64>()
+        .unwrap();
+    let sync_record = sync_record_dao::get_sync_record(stock).await?;
+    let mut updated: bool = false;
+    if let Some(sync_record) = sync_record {
+        updated = sync_record.date == date && sync_record.updated;
+    }
+
+    let prices = if updated {
+        let prices = stock_price_dao::get_stock_prices(stock).await?;
+        let now = Utc::now().with_timezone(&exchange.time_zone());
+        let seconds = 3600 * 24 - now.num_seconds_from_midnight();
+        con.set_ex::<&str, String, String>(
+            &key,
+            serde_json::to_string(&prices).unwrap(),
+            seconds as u64,
+        )?;
+        prices
+    } else {
+        Vec::new()
+    };
+
+    Ok(prices)
 }
 
 pub async fn get_stock_daily_price(
