de6db7621221ef0afdb701234c97ed7304faba8f
caijh <caiqizhe@gmail.com>
2025-05-31 21:53:51 +0800

    Update sql

--- sql/data.sql
+++ sql/data.sql
@@ -16,3 +16,18 @@ insert into stock(code, name, exchange, stock_type, stock_code)
 values ('NDX.NS', '纳斯达克指数', 'NASDAQ', 'Index', 'NDX');
 insert into stock(code, name, exchange, stock_type, stock_code)
 values ('SPX.NS', '标普500指数', 'NASDAQ', 'Index', 'SPX');
+
+INSERT INTO stock.market_time (id, exchange, start_time, end_time)
+VALUES (1, 'SSE', '09:30:00', '11:30:00');
+INSERT INTO stock.market_time (id, exchange, start_time, end_time)
+VALUES (2, 'SSE', '13:00:00', '15:00:00');
+INSERT INTO stock.market_time (id, exchange, start_time, end_time)
+VALUES (3, 'SZSE', '09:30:00', '11:30:00');
+INSERT INTO stock.market_time (id, exchange, start_time, end_time)
+VALUES (4, 'SZSE', '13:00:00', '15:00:00');
+INSERT INTO stock.market_time (id, exchange, start_time, end_time)
+VALUES (5, 'HKEX', '09:30:00', '12:00:00');
+INSERT INTO stock.market_time (id, exchange, start_time, end_time)
+VALUES (6, 'HKEX', '13:00:00', '16:00:00');
+INSERT INTO stock.market_time (id, exchange, start_time, end_time)
+VALUES (7, 'NASDAQ', '09:30:00', '16:00:00');
