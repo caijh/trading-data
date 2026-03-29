c29908f26d14e519de6af9122df3e6b03c8e09d6
caijh <caiqizhe@gmail.com>
2026-03-29 19:43:03 +0800

    feat(database): 更新与扩展股票和指数数据表
    - 调整数据结构以优化股票与指数数据的插入与存储
    - 添加新的股票和指数记录，包括标普500和中证500动态指数
    - 提高了系统对新类型股票和指数的支持，增强数据的多样性和完整性

--- sql/data.sql
+++ sql/data.sql
@@ -4,9 +4,7 @@ values ('000016.SH', '上证50', 'SSE', '000016'),
        ('HSI.HK', '恒生指数', 'HKEX', 'hsi'),
        ('NDX.NS', '纳斯达克100', 'NASDAQ', 'NDX')
 ;
-insert into stock_index(code, name, exchange, index_code)
-values ('SPX.NS', 'S&P 500', 'NASDAQ', 'SPX')
-;
+
 insert into stock(code, name, exchange, stock_type, stock_code)
 values ('000001.SH', '上证指数', 'SSE', 'Index', '000001');
 insert into stock(code, name, exchange, stock_type, stock_code)
@@ -17,8 +15,16 @@ insert into stock(code, name, exchange, stock_type, stock_code)
 values ('HSI.HK', '恒生指数', 'HKEX', 'Index', 'HSI');
 insert into stock(code, name, exchange, stock_type, stock_code)
 values ('NDX.NS', '纳斯达克指数', 'NASDAQ', 'Index', 'NDX');
+
+insert into stock_index(code, name, exchange, index_code)
+values ('SPX.NS', 'S&P 500', 'NASDAQ', 'SPX');
 insert into stock(code, name, exchange, stock_type, stock_code)
 values ('SPX.NS', '标普500指数', 'NASDAQ', 'Index', 'SPX');
+
+insert into stock_index(code, name, exchange, index_code)
+values ('000905.SH', '中证500动态指数', 'SSE', 'H30087');
+insert into stock(code, name, exchange, stock_type, stock_code)
+values ('000905.SH', '中证500动态指数', 'SSE', 'Index', '000905');
 
 INSERT INTO stock.market_time (id, exchange, start_time, end_time)
 VALUES (1, 'SSE', '09:30:00', '11:30:00');
